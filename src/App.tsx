5927de4ca1e1d910034c5a5e517a433ed81a3b7b
Jonathan Larsson <jonathan.larsson@iths.se>
2024-03-19 21:47:41 +0100

    G2-36: Clean code

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,20 @@
 import "./App.css";
-import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import About from "./pages/About";
 import ShoppingCart from "./pages/ShoppingCart";
 import Navbar from "./pages/Navbar";
 import Footer from "./pages/Footer";
 
-const Layout = () => {
-  return (
-    <div>
-      <Navbar />
-      <Outlet />
-      {/* Lägg till <Footer/> här */}
-    </div>
-  );
-};
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        path: "/",
-        element: <Index />,
-      },
-      {
-        path: "/about",
-        element: <About />,
-      },
-      {
-        path: "/shoppingcart",
-        element: <ShoppingCart />,
-      },
-    ],
-  },
-]);
-
 function App() {
   return (
     <>
-      <div>
-        <RouterProvider router={router} />
-      </div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/shoppingcart" element={<ShoppingCart />} />
+      </Routes>
     </>
   );
 }
