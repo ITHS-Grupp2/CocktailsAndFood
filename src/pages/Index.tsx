441aacbfa0217f089873636ef55e7bae1a842d6b
Alexander Råsberg <alexander.rasberg@iths.se>
2024-03-20 14:48:21 +0100

    G2-37 Added blocks for the ads on the siede, index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -4,48 +4,61 @@ export const Index = () => {
   return (
     <>
       <h3>Most popular burgers</h3>
-      <Container className="m-2">
-        <Row md={3} xs={2} className="g-3">
-          <Col>
-            <Card>Burger 1</Card>
-          </Col>
-          <Col>
-            <Card>Burger 2</Card>
-          </Col>
-          <Col>
-            <Card>Burger 3</Card>
-          </Col>
-          <Col>
-            <Card>Burger 4</Card>
-          </Col>
-          <Col>
-            <Card>Burger 5</Card>
-          </Col>
-          <Col>
-            <Card>Burger 6</Card>
-          </Col>
-        </Row>
-      </Container>
-      <Container className="m-2">
-        <Placeholder
-          bg="primary"
-          className="d-flex justify-content-center my-2"
-        >
-          <h4>Cocktails of the week</h4>
-        </Placeholder>
-        <Row xs={2} md={3} className="g-3">
-          <Col>
-            <Card>Cocktail 1</Card>
-          </Col>
-          <Col>
-            <Card>Cocktail 2</Card>
-          </Col>
-          <Col>
-            <Card>Cocktail 3</Card>
-          </Col>
-        </Row>
-      </Container>
-      <div></div>
+      <Row>
+        <Col>
+          <div className="h-100 bg-danger" style={{ width: "100px" }}>
+            Film
+          </div>
+        </Col>
+        <Col md="auto">
+          <Container className="m-2">
+            <Row md={3} xs={2} className="g-3">
+              <Col>
+                <Card>Burger 1</Card>
+              </Col>
+              <Col>
+                <Card>Burger 2</Card>
+              </Col>
+              <Col>
+                <Card>Burger 3</Card>
+              </Col>
+              <Col>
+                <Card>Burger 4</Card>
+              </Col>
+              <Col>
+                <Card>Burger 5</Card>
+              </Col>
+              <Col>
+                <Card>Burger 6</Card>
+              </Col>
+            </Row>
+          </Container>
+          <Container className="m-2">
+            <Placeholder
+              bg="primary"
+              className="d-flex justify-content-center my-2"
+            >
+              <h4>Cocktails of the week</h4>
+            </Placeholder>
+            <Row xs={2} md={3} className="g-3">
+              <Col>
+                <Card>Cocktail 1</Card>
+              </Col>
+              <Col>
+                <Card>Cocktail 2</Card>
+              </Col>
+              <Col>
+                <Card>Cocktail 3</Card>
+              </Col>
+            </Row>
+          </Container>
+        </Col>
+        <Col>
+          <div className="h-100 bg-danger" style={{ width: "100px" }}>
+            Film
+          </div>
+        </Col>
+      </Row>
     </>
   );
 };
