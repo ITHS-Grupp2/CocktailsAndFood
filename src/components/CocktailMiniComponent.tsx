31c370e4ce9fa4d5c3ee6f4279317579d308e012
crebane <carolinerebane@gmail.com>
2024-03-26 14:52:39 +0100

    Made the Cards the same size and styled the page.
    Co-authored-by: rickzt <rickzt@users.noreply.github.com>

--- src/components/CocktailMiniComponent.tsx
+++ src/components/CocktailMiniComponent.tsx
@@ -1,35 +1,34 @@
-import { Button, Card } from "react-bootstrap";
-import { Cocktail, price } from "../API/CocktailFetch";
-import { Link } from "react-router-dom";
-import { NavigationButton } from "./NavigationButton";
+import { Button, Card } from "react-bootstrap"
+import { Cocktail, price } from "../API/CocktailFetch"
+import { Link } from "react-router-dom"
+import { NavigationButton } from "./NavigationButton"
 
-export const CocktailMiniComponent = (
-  data: { cocktail: Cocktail}) => {
-  return (
-    <Card className="h-100" style={{ width: "15rem", overflow: "hidden" }}>
-      <Card.Img
-        variant="top"
-        src={data.cocktail.img}
-        style={{
-          objectFit: "cover",
-        }}
-      />
-      <Card.Body
-        className="d-flex flex-column mb-2"
-        style={{ padding: "0px 10px" }}
-      >
-        <Card.Title className="d-flex justify-content-between align-items-baseline mb-1">
-          <span className="fs-5">{data.cocktail.name}</span>
-          {/* Spannet kan inte ha för lång text utan mellanrum, då "väller" det ut */}
-          <span
-            className="text-muted align-self-end"
-            style={{ minWidth: "4rem", textAlign: "right" }}
-          >
-            {price} $
-          </span>
-        </Card.Title>
-      </Card.Body>
-      <NavigationButton navigationPath="/shoppingcart"></NavigationButton>
-    </Card>
-  );
-};
+export const CocktailMiniComponent = (data: { cocktail: Cocktail }) => {
+	return (
+		<Card
+			className="h-100"
+			style={{ width: "15rem", overflow: "hidden" }}>
+			<Card.Img
+				variant="top"
+				src={data.cocktail.img}
+				style={{
+					objectFit: "cover",
+				}}
+			/>
+			<Card.Body
+				className="d-flex flex-column mb-2"
+				style={{ padding: "0px 10px" }}>
+				<Card.Title className="d-flex justify-content-between align-items-baseline mb-1">
+					<span className="fs-5">{data.cocktail.name}</span>
+					{/* Spannet kan inte ha för lång text utan mellanrum, då "väller" det ut */}
+					<span
+						className="text-muted align-self-end"
+						style={{ minWidth: "4rem", textAlign: "right" }}>
+						{price} $
+					</span>
+				</Card.Title>
+			</Card.Body>
+			<NavigationButton navigationPath="/shoppingcart"></NavigationButton>
+		</Card>
+	)
+}
--- src/components/SidesComponent.tsx
+++ src/components/SidesComponent.tsx
@@ -1,34 +1,61 @@
-import { Card, Col, Container, Row } from "react-bootstrap";
-import { FoodAPI } from "../API/FoodAPI";
-import { NavigationButton } from "./NavigationButton";
+import { Card, Col, Container, Row } from "react-bootstrap"
+import { FoodAPI } from "../API/FoodAPI"
+import { NavigationButton } from "./NavigationButton"
 
 // En funktion som hämtar alla sides och populerar data-variabeln med dessa.
 export const SidesComponent = () => {
-  const sides = FoodAPI("sides");
-  return (
-    <Container>
-      <h2>Select sides</h2>
-      <Row>
-        {sides.map((sides, index) => (
-          <Col key={index} sm={6} md={4} lg={3} className="mb-4">
-            <Card>
-              <Card.Img variant="top" src={sides.imageUrl} />
-              <Card.Body>
-                <Card.Title className="d-flex justify-content-between align-items-baseline mb-1">
-                  <span className="fs-5 ">{sides.title} </span>
-                  <span
-                    className="text-muted align-self-end"
-                    style={{ minWidth: "4rem", textAlign: "right" }}
-                  >
-                    ${sides.price}
-                  </span>
-                </Card.Title>
-              </Card.Body>
-            </Card>
-            <NavigationButton navigationPath="/drinkselect"></NavigationButton>
-          </Col>
-        ))}
-      </Row>
-    </Container>
-  );
-};
+	const sides = FoodAPI("sides")
+	return (
+		<Container>
+			<div
+				className="d-flex justify-content-center align-items-center bg-light rounded shadow-sm my-3"
+				style={{ fontFamily: "Arial, sans-serif", height: "100px" }}>
+				<h1
+					className="text-center"
+					style={{ marginBottom: "0px" }}>
+					Select Sides
+				</h1>
+			</div>
+			<Row>
+				{sides.map((sides, index) => (
+					<Col
+						key={index}
+						// sm={6}
+						// md={4}
+						// lg={3}
+						className="mb-4">
+						<Card
+							key={index}
+							className="h-100 shadow"
+							style={{
+								width: "15rem",
+								overflow: "hidden",
+							}}>
+							<Card.Img
+								variant="top"
+								src={sides.imageUrl}
+								style={{
+									objectFit: "cover",
+									height: "200px",
+								}}
+							/>
+							<Card.Body
+								className="d-flex flex-column mb-2 "
+								style={{ padding: "0px 10px", height: "3rem" }}>
+								<Card.Title className="d-flex justify-content-between align-items-baseline mb-1">
+									<span className="fs-5 ">{sides.title} </span>
+									<span
+										className="text-muted align-self-end"
+										style={{ minWidth: "4rem", textAlign: "right" }}>
+										${sides.price}
+									</span>
+								</Card.Title>
+							</Card.Body>
+							<NavigationButton navigationPath="/drinkselect"></NavigationButton>
+						</Card>
+					</Col>
+				))}
+			</Row>
+		</Container>
+	)
+}
