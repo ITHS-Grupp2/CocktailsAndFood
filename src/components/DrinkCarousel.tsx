e67ff3bb7d065ec75c738716612551f507e09cee
Jonathan Larsson <jonathan.larsson@iths.se>
2024-03-27 09:51:26 +0100

    Fixade storlek på bilderna/hastigheten
    Co-authored-by: rickzt <rickzt@users.noreply.github.com>
    Co-authored-by: AnnaLindroos <AnnaLindroos@users.noreply.github.com>

--- src/components/DrinkCarousel.tsx
+++ src/components/DrinkCarousel.tsx
@@ -1,17 +1,18 @@
 import "react-multi-carousel/lib/styles.css";
 import Carousel from "react-multi-carousel";
 import { Link } from "react-router-dom";
+import { Cocktail } from "../API/CocktailFetch";
 
 const responsive = {
   desktop: {
     breakpoint: { max: 3000, min: 1024 },
-    items: 3,
+    items: 4,
     slidesToSlide: 1,
   },
   tablet: {
     breakpoint: { max: 1024, min: 768 },
-    items: 3,
-    slidesToSlide: 3,
+    items: 2,
+    slidesToSlide: 1,
   },
   mobile: {
     breakpoint: { max: 767, min: 464 },
@@ -20,42 +21,48 @@ const responsive = {
   },
 };
 
-type Cocktail = {
-  url: string;
-  name: string;
-  price: number;
-};
-
 const cocktailUrl: Cocktail[] = [
   {
-    url: "https://www.thecocktaildb.com/images/media/drink/mx31hv1487602979.jpg",
+    img: "https://www.thecocktaildb.com/images/media/drink/mx31hv1487602979.jpg",
     name: "Lemon Shot",
-    price: 12,
+    id: "",
+    instructions: "",
+    ingredients: [],
   },
   {
-    url: "https://www.thecocktaildb.com/images/media/drink/nzlyc81605905755.jpg",
+    img: "https://www.thecocktaildb.com/images/media/drink/nzlyc81605905755.jpg",
     name: "Gin & Soda",
-    price: 8,
+    id: "",
+    instructions: "",
+    ingredients: [],
   },
   {
-    url: "https://www.thecocktaildb.com/images/media/drink/7cll921606854636.jpg",
+    img: "https://www.thecocktaildb.com/images/media/drink/7cll921606854636.jpg",
     name: "Tom Collins",
-    price: 7,
+    id: "",
+    instructions: "",
+    ingredients: [],
   },
   {
-    url: "https://www.thecocktaildb.com/images/media/drink/qgdu971561574065.jpg",
+    img: "https://www.thecocktaildb.com/images/media/drink/qgdu971561574065.jpg",
     name: "Negroni",
-    price: 9,
+    id: "",
+    instructions: "",
+    ingredients: [],
   },
   {
-    url: "https://www.thecocktaildb.com/images/media/drink/drtihp1606768397.jpg",
+    img: "https://www.thecocktaildb.com/images/media/drink/drtihp1606768397.jpg",
     name: "Gin Fizz",
-    price: 11,
+    id: "",
+    instructions: "",
+    ingredients: [],
   },
   {
-    url: "https://www.thecocktaildb.com/images/media/drink/qyxrqw1439906528.jpg",
+    img: "https://www.thecocktaildb.com/images/media/drink/qyxrqw1439906528.jpg",
     name: "Vodka Martini",
-    price: 15,
+    id: "",
+    instructions: "",
+    ingredients: [],
   },
 ];
 
@@ -69,23 +76,30 @@ const DrinkCarousel = () => {
         draggable={false}
         infinite={true}
         partialVisible={false}
+        transitionDuration={2000}
+        removeArrowOnDeviceType={["tablet", "mobile", "desktop"]}
         centerMode={true}
         autoPlaySpeed={2000}
       >
         {cocktailUrl.map((imageUrl, index) => {
           return (
-            <div key={index}>
-              <Link to="/drinkselect">
-                <img
-                  src={imageUrl.url}
-                  style={{
-                    width: "250px",
-                    height: "250px",
-                  }}
-                />
-              </Link>
+            <div
+              key={index}
+              style={{
+                width: "70%",
+              }}
+            >
+              {/* <Link to="/drinkselect">  */}
+              <img
+                src={imageUrl.img}
+                style={{
+                  width: "100%",
+                  height: "100%",
+                }}
+              />
+              {/* </Link> */}
               <h3>{`${imageUrl.name}`}</h3>
-              <p>{`${imageUrl.price}$`}</p>
+              <p>$9.99</p>
             </div>
           );
         })}
