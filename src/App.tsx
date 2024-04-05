e654397145d0f32b8003e00ba83d6379f349adee
Jonathan Larsson <jonathan.larsson@iths.se>
2024-04-05 15:09:09 +0200

    G2-102: Skapa en home-knapp & About-knapp som navigerar en till sidan

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import { Index } from "./pages/Index";
 import { About } from "./pages/About";
 import { ShoppingCart } from "./pages/ShoppingCart";
-import { Navbar } from "./components/Navbar";
+import { Navbar, LiveNavbar } from "./components/Navbar";
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 import { SideSelect } from "./pages/SideSelect";
@@ -11,13 +11,14 @@ import { DrinkSelect } from "./pages/DrinkSelect";
 import { ProductInfoView } from "./pages/ProductInfoView";
 
 function App() {
-  const foodId = 3
+  const foodId = 3;
   return (
     <>
-    <div id="header">
-      <Header />
-      <Navbar />
-    </div>
+      <div id="header">
+        <Header />
+        <Navbar />
+        <LiveNavbar />
+      </div>
       <Routes>
         <Route path="/" element={<Index />}></Route>
         <Route path="/about" element={<About />} />
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,9 +9,36 @@ export const Navbar = () => {
           <Link to="/">Home</Link>
         </li>
         <li className="navbar-item">
-          <Link to="/about">About us</Link>
+          <Link to="/productinfoview">Product Info</Link>
+        </li>
+        <li className="navbar-item">
+          <Link to="/sideselect">Side Select</Link>
+        </li>
+        <li className="navbar-item">
+          <Link to="/drinkselect">Drink Select</Link>
+        </li>
+        <li className="navbar-item">
+          <Link to="/shoppingcart">Shopping Cart</Link>
         </li>
       </ul>
     </nav>
+  );
+};
+
+export const LiveNavbar = () => {
+  return (
+    <>
+      <h4>Live navbar: </h4>
+      <nav className="navbar">
+        <ul className="navbar-list">
+          <li className="navbar-item">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="navbar-item">
+            <Link to="/about">About us</Link>
+          </li>
+        </ul>
+      </nav>
+    </>
   );
 };
