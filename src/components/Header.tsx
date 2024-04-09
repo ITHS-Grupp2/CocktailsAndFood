774e00d954472596934a6e888aa9f204f24dbb2b
Jonathan Larsson <jonathan.larsson@iths.se>
2024-04-09 21:27:10 +0200

    Shopping-Icon klar
    Co-authored-by: Alexander Råsberg <alexander-ras@users.noreply.github.com>

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,23 +2,25 @@ import { Link } from "react-router-dom";
 export const Header = () => {
   return (
     <>
-      <div className="w-100 headerTotal">
-        <Link to="/" style={{ marginLeft: "40%" }}>
+      <div className="headerTotal">
+        <Link to="/">
           <h1 className="h1Header">Cocktails & Food</h1>
         </Link>
-        <Link to="/shoppingcart" style={{ marginRight: "19%" }}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 576 512"
-            width="30px"
-            height="30px"
-          >
-            <path
-              fill="white"
-              d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"
-            />
-          </svg>
-        </Link>
+        <div className="shoppingIcon">
+          <Link to="/shoppingcart">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 576 512"
+              width="33px"
+              height="33px"
+            >
+              <path
+                fill="white"
+                d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"
+              />
+            </svg>
+          </Link>
+        </div>
       </div>
     </>
   );
