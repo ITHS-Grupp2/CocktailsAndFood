36e1a9111dbfc9e84c81413318f3ebb4aa094196
Jonathan Larsson <jonathan.larsson@iths.se>
2024-04-10 22:21:22 +0200

    Fixa utseende och logik för shoppingcart
    Co-authored-by: Alexander Råsberg <alexander-ras@users.noreply.github.com>
    Co-authored-by: AnnaLindroos <AnnaLindroos@users.noreply.github.com>
    Co-authored-by: Kruxo <Kruxo@users.noreply.github.com>

--- src/components/CartProductList.tsx
+++ src/components/CartProductList.tsx
@@ -1,47 +1,62 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { CartContext } from "./CartContext";
 import { ShoppingAmount } from "./ShoppingAmount";
 
 export const CartProductList = () => {
   const state = useContext(CartContext);
+
+  const [totalPrice, setTotalPrice] = useState(0);
+
+  useEffect(() => {
+    let totalPrice = state.cartProducts.reduce(
+      (count, cartProduct) => count + cartProduct.price, // * cartProduct.quantity,
+      0
+    );
+    setTotalPrice(totalPrice);
+  }, [state.cartProducts]);
+
   return (
-    <div className="container">
-      {state.cartProducts.length > 0 ? (
-        <div className="row">
-          <div className="col">
-            <div className="card mb-3">
-              <div className="card-header">
-                <h5 className="card-title">Your Cart</h5>
-              </div>
-              <div className="card-body">
-                {state.cartProducts.map((cartProduct, index) => (
-                  <div key={index} className="row mb-3">
-                    <div className="col-3" style={{ maxWidth: "200px" }}>
-                      <img
-                        src={cartProduct.img}
-                        alt={cartProduct.title}
-                        className="img-fluid"
-                      />
+    <>
+      <div className="container">
+        {state.cartProducts.length > 0 ? (
+          <div className="row">
+            <div className="col">
+              <div className="card mb-3">
+                <div className="card-body">
+                  {state.cartProducts.map((cartProduct, index) => (
+                    <div key={index} className="row mb-3">
+                      <div className="col-3" style={{ maxWidth: "200px" }}>
+                        <img
+                          src={cartProduct.img}
+                          alt={cartProduct.title}
+                          className="img-fluid"
+                        />
+                      </div>
+                      <div className="col">
+                        <h6 className="card-subtitle mb-2">
+                          {cartProduct.title}
+                        </h6>
+                        <p className="card-text">${cartProduct.price}</p>
+                        <ShoppingAmount productId={cartProduct.id} />
+                      </div>
                     </div>
-                    <div className="col">
-                      <h6 className="card-subtitle mb-2">
-                        {cartProduct.title}
-                      </h6>
-                      <p className="card-text">${cartProduct.price}</p>
-                      <ShoppingAmount productId={cartProduct.id} />
-                    </div>
-                  </div>
-                ))}
+                  ))}
+                </div>
               </div>
-            </div>
-            <div className="text-end">
-              <button className="btn btn-primary">Checkout</button>
+              <div>
+                <h6 className="text-end" style={{ color: "white" }}>
+                  Total price: ${totalPrice.toFixed(2)}
+                </h6>
+              </div>
+              <div className="text-end">
+                <button className="btn btn-success">Pay</button>
+              </div>
             </div>
           </div>
-        </div>
-      ) : (
-        <p>Your cart is empty</p>
-      )}
-    </div>
+        ) : (
+          <p>Your cart is empty</p>
+        )}
+      </div>
+    </>
   );
 };
--- src/components/ShoppingAmount.tsx
+++ src/components/ShoppingAmount.tsx
@@ -13,21 +13,17 @@ export const ShoppingAmount = (props: { productId: string }) => {
       case ACTIONS.INCREMENT:
         return state + 1;
       case ACTIONS.DECREMENT:
-        if (state === 0) {
-          return state;
-        } else return state - 1;
+        return state - 1;
       default:
         return state;
     }
   };
-
   const [amount, dispatch] = useReducer(reducer, 1);
   const dispatchCart = useContext(CartDispatchContext);
 
   return (
     <>
       <div>
-        <p>id: {props.productId}</p>
         <Button
           onClick={() => {
             if (amount !== 1) {
@@ -43,7 +39,9 @@ export const ShoppingAmount = (props: { productId: string }) => {
         </Button>
         <span>{amount}</span>
         <Button
-          onClick={() => dispatch({ type: ACTIONS.INCREMENT })}
+          onClick={() => {
+            dispatch({ type: ACTIONS.INCREMENT });
+          }}
           style={{ marginLeft: "15px" }}
           className="rounded-circle"
         >
