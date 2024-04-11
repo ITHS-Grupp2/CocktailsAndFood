1e0f0b6202a563da3b16c3ca9cfa819c4aa06f5a
Alexander Råsberg <alexander.rasberg@iths.se>
2024-04-11 22:49:11 +0200

    Shoppingcart CSS Fixed as Figma

--- src/components/CartProductList.tsx
+++ src/components/CartProductList.tsx
@@ -22,26 +22,28 @@ export const CartProductList = () => {
 
   return (
     <>
-      <div className="container">
+      <div className="cartItemListBox">
         {state.cartProducts.length > 0 ? (
-          <div className="col">
-            <div className="col card mb-3">
-              <div className="card-body">
-                {state.cartProducts.map((cartProduct, index) => (
-                  <div key={index} className="row mb-3">
-                    <div className="col-3" style={{ maxWidth: "200px" }}>
-                      <img
-                        src={cartProduct.img}
-                        alt={cartProduct.title}
-                        className="img-fluid"
-                      />
-                    </div>
-                    <div className="col">
-                      <h6 className="card-subtitle mb-2">
-                        {cartProduct.title}
-                      </h6>
-                      <p className="card-text">${cartProduct.price}</p>
-                      {/*När antalet ändras, anropas updateQuantity-funktionen i CartContext. */}
+          <div className="col ">
+            <div className="col cardItemList">
+              {state.cartProducts.map((cartProduct, index) => (
+                <div
+                  key={index}
+                  className="row mt-3"
+                  style={{ borderBottom: "3px solid black" }}
+                >
+                  <div className="col-3" style={{ maxWidth: "200px" }}>
+                    <img
+                      src={cartProduct.img}
+                      alt={cartProduct.title}
+                      className="img-fluid"
+                    />
+                  </div>
+                  <div className="col">
+                    <h6 className="card-subtitle mb-2">{cartProduct.title}</h6>
+                    <p className="card-text">${cartProduct.price}</p>
+                    {/*När antalet ändras, anropas updateQuantity-funktionen i CartContext. */}
+                    <div className="text-end" style={{ marginTop: "55px" }}>
                       <ShoppingAmount
                         productId={cartProduct.id}
                         quantity={cartProduct.quantity}
@@ -51,16 +53,20 @@ export const CartProductList = () => {
                       />
                     </div>
                   </div>
-                ))}
-              </div>
+                </div>
+              ))}
             </div>
-            <div>
-              <h6 className="text-end" style={{ color: "white" }}>
-                Total price: ${totalPrice.toFixed(2)}
+            <div className="cartProductListBottom">
+              <h6
+                className="text-end"
+                style={{ color: "white", marginRight: "30px" }}
+              >
+                {state.cartProducts.length} products in cart | Total price: $
+                {totalPrice.toFixed(2)}
               </h6>
             </div>
             <div className="text-end">
-              <button className="btn btn-success">Pay</button>
+              <button className="cartProductCheckoutButton">Pay</button>
             </div>
           </div>
         ) : (
