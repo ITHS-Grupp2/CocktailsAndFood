4aca6e85cbd43dca3310b6687d9d7e02f4fa9242
Alexander Råsberg <alexander.rasberg@iths.se>
2024-04-11 20:47:02 +0200

    Fixat boxen på about us

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,13 +2,23 @@ import { Card, Col, Container, Row } from "react-bootstrap";
 import data from "../assets/people.json";
 
 export const About = () => {
-  function Test(firstName: string, lastName: string, nickName: string, imgsrc: string) {
+  const itpRows = [];
+  const devRows = [];
+
+  function GetPersonCard(
+    firstName: string,
+    lastName: string,
+    nickName: string,
+    imgsrc: string
+  ) {
     return (
       <Card
         style={{
           overflow: "hidden",
           padding: "0px",
-        }}>
+          height: "385px",
+        }}
+      >
         <Card.Img
           src={imgsrc}
           variant="top"
@@ -17,7 +27,8 @@ export const About = () => {
             objectFit: "cover",
             height: "250px",
             width: "auto",
-          }}></Card.Img>
+          }}
+        ></Card.Img>
         <Card.Body>
           <Card.Title>
             <p>
@@ -25,19 +36,19 @@ export const About = () => {
               <span> </span>
               {lastName}
             </p>
-            <p><i>{nickName}</i></p>
+            <p>
+              <i>{nickName}</i>
+            </p>
           </Card.Title>
         </Card.Body>
       </Card>
     );
   }
-  const itpRows = [];
 
-  const devRows = [];
   for (let i = 0; i < data.developers.length; i++) {
     devRows.push(
       <Col>
-        {Test(
+        {GetPersonCard(
           data.developers[i].firstName,
           data.developers[i].lastName,
           data.developers[i].nickName,
@@ -49,7 +60,7 @@ export const About = () => {
   for (let i = 0; i < data.projectLeaders.length; i++) {
     itpRows.push(
       <Col>
-        {Test(
+        {GetPersonCard(
           data.projectLeaders[i].firstName,
           data.projectLeaders[i].lastName,
           data.projectLeaders[i].nickName,
@@ -59,21 +70,32 @@ export const About = () => {
     );
   }
   return (
-    <Container>
-      <h1>About</h1>
-      <Container className="align-items-center">
+    <>
+      <div className="headerSmaller" style={{ margin: "30px 0px" }}>
+        <h4 className="text-center h1Header" style={{ marginBottom: "0px" }}>
+          About Us
+        </h4>
+      </div>
+      <Container>
         <h4>Project Leaders</h4>
-        <Row lg={4} md={3} xs={2} className="g-3 p-2">
+        <Row lg={4} md={3} xs={2} className="g-3 p-2 justify-content-center">
           {itpRows}
         </Row>
         <Row>
           <h4>Developers</h4>
         </Row>
 
-        <Row lg={4} md={3} xs={2} className="g-3 p-2">
+        <Row lg={4} md={3} xs={2} className="g-3 p-2 justify-content-center">
           {devRows}
         </Row>
+        <Row className="g-3 p-2 justify-content-center">
+          <div className="aboutUsBox">
+            Hello there fiddling sizzle nizzlers. We are the crazy dev-team 
+            creating this insane burger-site just for your hungry needs.
+            <br/><br/><br/><br/>We can do backflips.
+          </div>
+        </Row>
       </Container>
-    </Container>
+    </>
   );
 };
