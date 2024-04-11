72ec915d225a2ffed58bbd0abeffb75f09515d53
Alexander Råsberg <alexander.rasberg@iths.se>
2024-04-11 15:15:18 +0200

    Cleaned up the code in Cocktailinfoservice

--- src/components/CocktailMiniComponent.tsx
+++ src/components/CocktailMiniComponent.tsx
@@ -1,7 +1,7 @@
 import { Card } from "react-bootstrap";
 import { Cocktail, price } from "../API/CocktailFetch";
 import { NavigationButton } from "./NavigationButton";
-import { CocktailPercentage } from "../services/CocktailInfoService";
+import { GetPercentage, MatchDrinkService } from "../services/CocktailInfoService";
 
 export const CocktailMiniComponent = (data: { cocktail: Cocktail }) => {
   return (
@@ -12,7 +12,8 @@ export const CocktailMiniComponent = (data: { cocktail: Cocktail }) => {
         overflow: "hidden",
         padding: "0px",
         margin: "5px",
-      }}>
+      }}
+    >
       <div style={{ overflow: "hidden" }}>
         <img
           className="cardImage"
@@ -26,19 +27,24 @@ export const CocktailMiniComponent = (data: { cocktail: Cocktail }) => {
       </div>
       <Card.Body
         className="d-flex flex-column mb-2"
-        style={{ padding: "0px 10px" }}>
+        style={{ padding: "0px 10px" }}
+      >
         <Card.Title className="d-flex justify-content-between align-items-baseline mb-1">
           <span className="fs-5">{data.cocktail.name}</span>
           {/* Spannet kan inte ha för lång text utan mellanrum, då "väller" det ut */}
           <span
             className="text-muted align-self-end"
-            style={{ minWidth: "4rem", textAlign: "right" }}>
+            style={{ minWidth: "4rem", textAlign: "right" }}
+          >
             ${price}
           </span>
         </Card.Title>
         <Card.Subtitle>
           <p>
-            <i>Alcohol Percentage: {CocktailPercentage(data.cocktail.id)}%</i>
+            <i>
+              Alcohol Percentage:{" "}
+              {GetPercentage(data.cocktail.id)}%
+            </i>
           </p>
         </Card.Subtitle>
       </Card.Body>
--- src/services/CocktailInfoService.ts
+++ src/services/CocktailInfoService.ts
@@ -1,89 +1,22 @@
-import motivationData from "../assets/drinkMotivation.json";
+import matchData from "../assets/drinkMatch.json";
 
 export type Drink = {
-    drinkId: number;
+    drinkId: string;
     percentage: number;
     motivation: string;
   };
-export const cocktailIdArray = [12752, 178342, 12402, 11003, 11410, 14167];
-export function CocktailPercentage(cocktail: number | string){
-    if (typeof cocktail === "number") {
-        switch (cocktail) {
-            case cocktailIdArray[0]: //Lemonshot
-                return 40;
-            case cocktailIdArray[1]: //Gin Soda
-                return 35;
-            case cocktailIdArray[2]: //Tom Collins
-                return 10;
-            case cocktailIdArray[3]: //Negroni
-                return 99;
-            case cocktailIdArray[4]: //Gin Fizz
-                return 15;
-            case cocktailIdArray[5]: //Vodka Martini
-                return 18;
-            default:
-                return 0;
-        }
-    } else {
-        switch (cocktail) {
-            case ""+cocktailIdArray[0]:
-                return 40;
-            case ""+cocktailIdArray[1]:
-                return 35;
-            case ""+cocktailIdArray[2]:
-                return 10;
-            case ""+cocktailIdArray[3]:
-                return 99;
-            case ""+cocktailIdArray[4]:
-                return 15;
-            case ""+cocktailIdArray[5]:
-                return 18;
-            default:
-                return 0;
-        }
-    }
-}
-  
 
-
+export const cocktailIdArray = ["12752", "178342", "12402", "11003", "11410", "14167"]; 
 
 export function MatchDrinkService(mealId: string) {
   const recommendedDrink: Drink = {
-    drinkId: -1,
-    percentage: -1,
-    motivation: " "
+    drinkId: matchData.drinkMatch.find((d) => d.burgerId === mealId)?.drinkId as string,
+    percentage: matchData.drinkMatch.find((d) => d.burgerId === mealId)?.percentage as number,
+    motivation: matchData.drinkMatch.find((d) => d.burgerId === mealId)?.motivation as string
   };
-  switch (mealId) {
-    case "65fd96c529f983c33c7ec9c2": //Gluten Free Burger With Bacon
-      recommendedDrink.drinkId = cocktailIdArray[5];
-      recommendedDrink.percentage = CocktailPercentage(recommendedDrink.drinkId);
-      recommendedDrink.motivation = motivationData.drinkMotivations.find((m) => m.id == mealId)?.motivation as string
-      break;
-    case "65fd96f229f983c33c7eca00": //Greasy Burger
-      recommendedDrink.drinkId = cocktailIdArray[3];
-      recommendedDrink.percentage = 35;
-      recommendedDrink.motivation = motivationData.drinkMotivations.find((m) => m.id == mealId)?.motivation as string
-      break;
-    case "65fd976a29f983c33c7eca37": //Chicken Burger
-      recommendedDrink.drinkId = cocktailIdArray[4];
-      recommendedDrink.percentage = 10;
-      recommendedDrink.motivation = motivationData.drinkMotivations.find((m) => m.id == mealId)?.motivation as string
-      break;
-    case "65fd978129f983c33c7eca55": //Olive Burger
-      recommendedDrink.drinkId = cocktailIdArray[2];
-      recommendedDrink.percentage = 99;
-      recommendedDrink.motivation = motivationData.drinkMotivations.find((m) => m.id == mealId)?.motivation as string
-      break;
-    case "65fd97b729f983c33c7eca9e": //Spicy Chicken Burger
-      recommendedDrink.drinkId = cocktailIdArray[1];
-      recommendedDrink.percentage = 15;
-      recommendedDrink.motivation = motivationData.drinkMotivations.find((m) => m.id == mealId)?.motivation as string
-      break;
-    case "65fd98ca29f983c33c7ece4a": //Healthy Burger with Salad
-      recommendedDrink.drinkId = cocktailIdArray[0];
-      recommendedDrink.percentage = 18;
-      recommendedDrink.motivation = motivationData.drinkMotivations.find((m) => m.id == mealId)?.motivation as string
-      break;
-  }
   return recommendedDrink;
+}
+
+export function GetPercentage(drinkId: string){
+  return matchData.drinkMatch.find((d) => d.drinkId === drinkId)?.percentage
 }
