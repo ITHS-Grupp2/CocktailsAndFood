15b11fd03b51905af530c1965d305f9211dbca62
Kruxo <142524330+Kruxo@users.noreply.github.com>
2024-04-12 13:18:45 +0200

    Changed let to const

--- src/pages/DrinkSelect.tsx
+++ src/pages/DrinkSelect.tsx
@@ -9,13 +9,13 @@ import {
 // const idArray = [12752, 178342, 12402, 11003, 11410, 14167];
 
 export function DrinkSelect() {
-  let foodId = localStorage.getItem("burgerId") as string;
+  const foodId = localStorage.getItem("burgerId") as string;
 
   const recommendedDrink = MatchDrinkService(foodId);
   const cocktail: Cocktail = CocktailFetch(recommendedDrink.drinkId);
   const cocktails: Cocktail[] = [];
   const labels: string[] = [];
-  let filteredCocktails = cocktailIdArray.filter(
+  const filteredCocktails = cocktailIdArray.filter(
     (drink) => drink !== recommendedDrink.drinkId
   );
   filteredCocktails.forEach((id) => {
@@ -26,14 +26,11 @@ export function DrinkSelect() {
   if (localStorage.getItem("burgerId") !== null) {
     return (
       <>
-          <div className="headerSmaller" style={{ margin: "30px 0px" }}>
-            <h1
-              className="text-center"
-              style={{ marginBottom: "0px" }}
-            >
-              Recommended Drink
-            </h1>
-          </div>
+        <div className="headerSmaller" style={{ margin: "30px 0px" }}>
+          <h1 className="text-center" style={{ marginBottom: "0px" }}>
+            Recommended Drink
+          </h1>
+        </div>
 
         <ProductInfo
           id={cocktail.id}
@@ -48,11 +45,11 @@ export function DrinkSelect() {
           motivation={recommendedDrink.motivation}
           quantity={1}
         ></ProductInfo>
-      <div className="headerSmaller" style={{ margin: "30px 0px" }}>
-        <h4 className="text-center" style={{ marginBottom: "0px" }}>
-          Other Drinks
-        </h4>
-      </div>
+        <div className="headerSmaller" style={{ margin: "30px 0px" }}>
+          <h4 className="text-center" style={{ marginBottom: "0px" }}>
+            Other Drinks
+          </h4>
+        </div>
         <div className="center-page-items">
           <CocktailPanel cocktails={cocktails} />
         </div>
@@ -61,14 +58,11 @@ export function DrinkSelect() {
   } else {
     return (
       <>
-          <div className="headerSmaller" style={{ margin: "30px 0px" }}>
-            <h1
-              className="text-center"
-              style={{ marginBottom: "0px" }}
-            >
-              Select Drink
-            </h1>
-          </div>
+        <div className="headerSmaller" style={{ margin: "30px 0px" }}>
+          <h1 className="text-center" style={{ marginBottom: "0px" }}>
+            Select Drink
+          </h1>
+        </div>
         <div className="center-page-items">
           <CocktailPanel cocktails={cocktails} />
         </div>
