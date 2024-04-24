8f4039cf7c50826b0e3cbc420810270b2ccfc316
AnnaLindroos <92397151+AnnaLindroos@users.noreply.github.com>
2024-04-24 14:00:10 +0200

    ChangedNoCardToButton
    Co-authored-by: Kruxo <Kruxo@users.noreply.github.com>
    Co-authored-by: crebane <crebane@users.noreply.github.com>
    Co-authored-by: Jonathan Larsson <j.o.n.a.t.h.a.n@hotmail.se>
    Co-authored-by: Alexander Råsberg <alexander-ras@users.noreply.github.com>

--- src/components/CocktailPanel.tsx
+++ src/components/CocktailPanel.tsx
@@ -8,7 +8,7 @@ export const CocktailPanel = (cocktailArray: { cocktails: Cocktail[] }) => {
   const rows = [];
   for (let i = 0; i < cocktailArray.cocktails.length; i++) {
     rows.push(
-      <Col>
+      <Col key={i}>
         <CocktailMiniComponent cocktail={cocktailArray.cocktails[i]} />
       </Col>
     );
@@ -24,54 +24,62 @@ export const CocktailPanel = (cocktailArray: { cocktails: Cocktail[] }) => {
       <Container className="m-2">
         <Row className="g-3">
           {rows}
-          <Col>
-            {/* conditional rendering, based on whether the burger ID is null or not  */}
-            {isBurgerIdNotNull && (
-              <Card
-                className="shadow"
-                style={{
-                  height: "421px",
-                  width: "300px",
-                  overflow: "hidden",
-                  padding: "0px",
-                  marginLeft: "15px",
-                  margin: "5px",
-                }}
-              >
-                <div style={{ overflow: "hidden" }}>
-                  <img
-                    className="cardImage"
-                    src={`https://img.freepik.com/premium-vector/drinking-alcoholic-beverages-forbidden-sign_204827-138.jpg`}
-                    alt={"No Drink"}
-                    style={{
-                      objectFit: "cover",
-                      height: "300px",
-                      width: "300px",
-                    }}
-                  />
-                </div>
-                <Card.Body
-                  className="d-flex flex-column mb-2"
-                  style={{ padding: "0px 10px" }}
+          {/* conditional rendering, based on whether the burger ID is null or not  */}
+          {isBurgerIdNotNull && (
+            <Col>
+              <div style={{ paddingLeft: "12px" }}>
+                <Card
+                  className="shadow"
+                  style={{
+                    height: "421px",
+                    width: "300px",
+                    overflow: "hidden",
+                    padding: "0px",
+                    marginLeft: "15px",
+                    margin: "5px",
+                  }}
                 >
-                  <Card.Title className="d-flex justify-content-between align-items-baseline mb-1">
-                    <span className="fs-5">No Drink</span>
-                  </Card.Title>
-                </Card.Body>
-                <Link to={`/shoppingcart`}>
-                  <button className="navigationButtonFill">
-                    {GetIcon("Cart", "white", "Medium")}{" "}
-                    <span style={{ paddingLeft: "10px" }}>No Drink</span>
-                  </button>
-                </Link>
-              </Card>
-            )}
-          </Col>
+                  <div style={{ overflow: "hidden" }}>
+                    <img
+                      className="cardImage"
+                      src={`https://img.freepik.com/premium-vector/drinking-alcoholic-beverages-forbidden-sign_204827-138.jpg`}
+                      alt={"No Drink"}
+                      style={{
+                        objectFit: "cover",
+                        height: "300px",
+                        width: "300px",
+                      }}
+                    />
+                  </div>
+                  <Card.Body
+                    className="d-flex flex-column mb-2"
+                    style={{ padding: "0px 10px" }}
+                  >
+                    <Card.Title className="d-flex justify-content-between align-items-baseline mb-1">
+                      <span className="fs-5">No Drink</span>
+                    </Card.Title>
+                  </Card.Body>
+                  <Link to={`/shoppingcart`}>
+                    <button className="navigationButtonFill">
+                      {GetIcon("Cart", "white", "Medium")}{" "}
+                      <span style={{ paddingLeft: "10px" }}>No Drink</span>
+                    </button>
+                  </Link>
+                </Card>
+              </div>
+            </Col>
+          )}
         </Row>
       </Container>
       {/* Button instead of the "no drink card" for when burgerId is null */}
       {!isBurgerIdNotNull && (
-        <div style={{ marginTop: "20px", textAlign: "center" }}>
+        <div
+          style={{
+            marginTop: "20px",
+            marginBottom: "20px",
+            textAlign: "center",
+          }}
+        >
           <Link to={`/shoppingcart`}>
             <button
               className="navigationButtonFill"
