d9d39e962374d94fb9c74bd5eff70ecc4e651318
Kruxo <142524330+Kruxo@users.noreply.github.com>
2024-04-26 12:09:35 +0200

    SASS

--- src/components/NextButton.tsx
+++ src/components/NextButton.tsx
@@ -13,26 +13,9 @@ export const NextButton: React.FC<NextButtonProps> = ({ targetPage }) => {
     >
       <div style={{ width: "30%", margin: "0 auto" }}>
         <Link to={targetPage}>
-          <button
-            style={{
-              backgroundColor: "#BF0426",
-              width: "100%",
-              padding: "0px",
-              border: "",
-              fontSize: "30px",
-              display: "flex",
-              justifyContent: "space-between", // Aligns items with space between them
-              alignItems: "center", // Centers items vertically
-              position: "relative",
-            }}
-          >
-            <span></span>
-            <span style={{ flex: "1", paddingTop: "5px", paddingLeft: "80px" }}>
-              NEXT
-            </span>
-            <span style={{ fontSize: "40px", paddingRight: "40px" }}>
-              &#129130;
-            </span>
+          <button className="nextButton">
+            <span className="nextButtonText">NEXT</span>
+            <span className="nextButtonArrow">&#129130;</span>
           </button>
         </Link>
       </div>
