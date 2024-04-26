3dae6eb34fead7ae5a7ae70a57f8f8ef643fd8a7
crebane <carolinerebane@gmail.com>
2024-04-26 09:25:22 +0200

    Added new "nav-buttons"

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,10 +6,16 @@ export const LiveNavbar = () => {
       <nav className="navbar">
         <ul className="navbar-list">
           <Link to="/">
-            <li className="navbar-item">Home</li>
+            <li className="navbar-item">Burgers</li>
+          </Link>
+          <Link to="/sideselect">
+            <li className="navbar-item">Sides</li>
+          </Link>
+          <Link to="/drinkselect">
+            <li className="navbar-item">Cocktails</li>
           </Link>
           <Link to="/about">
-            <li className="navbar-item">About us</li>
+            <li className="navbar-item">Soft Drinks</li>
           </Link>
         </ul>
       </nav>
