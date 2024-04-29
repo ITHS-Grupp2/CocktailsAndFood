e53aed3cc5835f997dcceb00f1551462cdf0e5f1
Kruxo <142524330+Kruxo@users.noreply.github.com>
2024-04-29 14:17:11 +0200

    Adjusting sizes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -16,15 +16,11 @@ export const Footer = () => {
       <div className="footer-social-icons">
         <img
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Facebook_icon_%28black%29.svg/240px-Facebook_icon_%28black%29.svg.png"
-          style={{ height: "40px" }}
-          className="p-1"
           alt="Facebook Icon"
         ></img>
         <br />
         <img
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/CIS-A2K_Instagram_Icon_%28Black%29.svg/240px-CIS-A2K_Instagram_Icon_%28Black%29.svg.png"
-          style={{ height: "40px" }}
-          className="p-1"
           alt="Instagram Icon"
         ></img>
       </div>
